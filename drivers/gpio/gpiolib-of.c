6ecd6d9e213aa9a2760ece99549d84f0164ade70
Xilin Wu <strongtz@yeah.net>
2023-07-29 11:30:50 +0800

    HACK: Add ST FTS V521 SPI touchscreen driver
    Signed-off-by: Xilin Wu <strongtz@yeah.net>

--- drivers/gpio/gpiolib-of.c
+++ drivers/gpio/gpiolib-of.c
@@ -30,15 +30,15 @@
  * match, but GPIO controllers are free to translate their own flags to
  * Linux-specific in their .xlate callback. Though, 1:1 mapping is recommended.
  */
-enum of_gpio_flags {
-	OF_GPIO_ACTIVE_LOW = 0x1,
-	OF_GPIO_SINGLE_ENDED = 0x2,
-	OF_GPIO_OPEN_DRAIN = 0x4,
-	OF_GPIO_TRANSITORY = 0x8,
-	OF_GPIO_PULL_UP = 0x10,
-	OF_GPIO_PULL_DOWN = 0x20,
-	OF_GPIO_PULL_DISABLE = 0x40,
-};
+// enum of_gpio_flags {
+// 	OF_GPIO_ACTIVE_LOW = 0x1,
+// 	OF_GPIO_SINGLE_ENDED = 0x2,
+// 	OF_GPIO_OPEN_DRAIN = 0x4,
+// 	OF_GPIO_TRANSITORY = 0x8,
+// 	OF_GPIO_PULL_UP = 0x10,
+// 	OF_GPIO_PULL_DOWN = 0x20,
+// 	OF_GPIO_PULL_DISABLE = 0x40,
+// };
 
 /**
  * of_gpio_named_count() - Count GPIOs for a device
@@ -381,6 +381,20 @@ out:
 
 	return desc;
 }
+
+int of_get_named_gpio_flags(struct device_node *np, const char *list_name,
+			    int index, enum of_gpio_flags *flags)
+{
+	struct gpio_desc *desc;
+
+	desc = of_get_named_gpiod_flags(np, list_name, index, flags);
+
+	if (IS_ERR(desc))
+		return PTR_ERR(desc);
+	else
+		return desc_to_gpio(desc);
+}
+EXPORT_SYMBOL_GPL(of_get_named_gpio_flags);
 
 /**
  * of_get_named_gpio() - Get a GPIO number to use with GPIO API
